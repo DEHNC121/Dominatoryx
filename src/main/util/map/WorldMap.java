860043da072c72faff68ce7058ce280f17d7dbcf
WiadroMasla <tomasz.kosmulski@student.uj.edu.pl>
2021-03-13 13:38:35 +0100

    start 2.3

--- src/main/util/map/WorldMap.java
+++ src/main/util/map/WorldMap.java
@@ -13,16 +13,26 @@ public class WorldMap {
 
     public WorldMap(PlayState.GameMapSize gameMapSize) {
 
-        GamePanel.width,GamePanel.height;
+        //GamePanel.width,GamePanel.height;
         widthHexagonNumber=gameMapSize.size.getKey();
         heightHexagonNumber=gameMapSize.size.getValue();
-
-        int hexagonPartsNumber=(6*widthHexagonNumber+1)*(2*heightHexagonNumber+1);
+        int hexagonPartsInRow=(3*widthHexagonNumber+1);
+        int hexagonPartsInColumn=(2*heightHexagonNumber+1);
+        int hexagonPartsNumber=hexagonPartsInRow*hexagonPartsInColumn;
         int hexagonNumber=widthHexagonNumber*heightHexagonNumber;
 
         Parts=new HexagonPart2D[hexagonPartsNumber];
-
-
+        int hexagonPartWidth=GamePanel.width/hexagonPartsInRow;
+        int hexagonPartHeight=GamePanel.height/hexagonPartsInColumn;
+        int hexagonPartWidthDiff=GamePanel.width/hexagonPartsInRow;
+        int hexagonPartHeightDiff=GamePanel.height/hexagonPartsInColumn;
+        for(int i=0;i<hexagonPartsInRow;i++){
+            for(int j=0;j<hexagonPartsInColumn;j++){
+                Parts[j*hexagonPartsInColumn+i]=
+                        new HexagonPart2D(i*hexagonPartWidthDiff,j*hexagonPartHeightDiff,
+                                hexagonPartWidth,hexagonPartHeight);
+            }
+        }
         hexagonMap=new Hexagon2D[hexagonNumber];
 
     }
