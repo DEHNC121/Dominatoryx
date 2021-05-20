acc5375d9cb947ba69585c59a6adc7f60835c558
radomil-baran <radomil.baran@gmail.com>
2021-05-20 19:14:55 +0200

    ScrollField

---
+++ src/main/graphics/ScrollField.java
@@ -0,0 +1,100 @@
+package main.graphics;
+
+import main.util.KeyHandler;
+import main.util.MouseHandler;
+import main.util.map.Object2DInt;
+import java.awt.*;
+
+import java.util.ArrayList;
+
+public class ScrollField {
+    String entry;
+    ArrayList<String> entries;
+    Object2DInt field;
+    Object2DInt wholeField;
+    DrawText textUp, textMiddle, textBottom;
+    int upIndex, middleIndex, bottomIndex;
+    boolean scrollUp, scrollDown;
+    public ScrollField(Object2DInt field, ArrayList<String> entries, String name, int style, int size, Color color) {
+        this.field = field;
+        this.entries = entries;
+        wholeField = new Object2DInt(field.getX(), field.getY(), field.getWidth(), 3 * field.getHeight());
+        middleIndex = 0;
+        bottomIndex = -1;
+        upIndex = (entries.size() > 1) ? 1 : -1;
+        textUp = new DrawText(name,stringFromIndex(upIndex), style, size,
+                new Object2DInt(field.getX(), field.getY() + field.getHeight(), field.getWidth(), field.getHeight()),
+                new Color(color.getRed(),color.getGreen(),color.getBlue(),40));
+        textMiddle = new DrawText(name,stringFromIndex(middleIndex), style, size,
+                new Object2DInt(field.getX(), field.getY() + 2*field.getHeight(), field.getWidth(), field.getHeight()),
+                new Color(color.getRed(),color.getGreen(),color.getBlue(),255));
+        textBottom = new DrawText(name,stringFromIndex(upIndex), style, size,
+                new Object2DInt(field.getX(), field.getY() + 3 * field.getHeight(), field.getWidth(), field.getHeight()),
+                new Color(color.getRed(),color.getGreen(),color.getBlue(),40));
+        entry = stringFromIndex(middleIndex);
+    }
+    public String getEntry () {
+        return this.entry;
+    }
+    public String stringFromIndex (int i) {
+        if (i == -1)
+            return null;
+        return entries.get(i);
+    }
+
+    public void update () {
+        if (scrollUp) {
+            scrollUp = false;
+            ifScrollUp();
+        }
+        if (scrollDown) {
+            scrollDown = false;
+            ifScrollDown();
+        }
+        textUp.setText(stringFromIndex(upIndex));
+        textBottom.setText(stringFromIndex(bottomIndex));
+        textMiddle.setText(stringFromIndex(middleIndex));
+        entry = stringFromIndex(middleIndex);
+    }
+
+    public void input (MouseHandler mouse, KeyHandler key) {
+        if (isMouseOn(mouse)) {
+            if (mouse.getRotation() < 0)
+                scrollUp = true;
+            if (mouse.getRotation() > 0)
+            scrollDown = true;
+        }
+        mouse.setRotation(0);
+    }
+    public void render (Graphics g) {
+        g.setColor(Color.GRAY);
+        if (textUp.getText() != null)
+            textUp.render(g);
+        if (textMiddle.getText() != null)
+            textMiddle.render(g);
+        if (textBottom.getText() != null)
+            textBottom.render(g);
+    }
+
+    public void ifScrollUp () {
+        if (middleIndex < entries.size() - 1) {
+            bottomIndex = middleIndex;
+            middleIndex = upIndex;
+            if (upIndex < (entries.size() - 1))
+                upIndex++;
+            else
+                upIndex = -1;
+        }
+    }
+    public void ifScrollDown () {
+        if (middleIndex > 0) {
+            upIndex = middleIndex;
+            middleIndex = bottomIndex;
+            bottomIndex = bottomIndex - 1;
+        }
+    }
+
+    public boolean isMouseOn (MouseHandler mouse) {
+        return wholeField.isInside(mouse.getX(), mouse.getY());
+    }
+}
--- src/main/states/MenuState.java
+++ src/main/states/MenuState.java
@@ -2,17 +2,20 @@ package main.states;
 
 import main.graphics.DrawText;
 import main.graphics.Image;
+import main.graphics.ScrollField;
 import main.util.KeyHandler;
 import main.util.MouseHandler;
 import main.GamePanel;
 import main.util.map.Object2DInt;
-
+import java.util.*;
 import java.awt.*;
 
 public class MenuState extends GameState {
     Image playImage;
     Image settingsImage;
     Image shutDownImage;
+    ScrollField sf;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
         playImage = new Image("initial/play_button.png");
@@ -21,10 +24,13 @@ public class MenuState extends GameState {
         playImage.setPositionCenter((int) GamePanel.width / 2, (int) GamePanel.height / 2);
         settingsImage.setPosition(20, 20);
         shutDownImage.setPosition((int) GamePanel.width - 20 - shutDownImage.getWidth(),20);
+        sf = new ScrollField(new Object2DInt(0, (int) GamePanel.height / 2, 100, 100),
+                new ArrayList<String>(Arrays.asList("1","2","3","4","5","6","7","8")), "TimesRoman", Font.PLAIN, 100,
+                new Color(0,0,0));
     }
     @Override
     public void update () {
-
+        sf.update();
     }
 
     @Override
@@ -43,6 +49,7 @@ public class MenuState extends GameState {
             }
             mouse.setIsClicked(false);
         }
+        sf.input(mouse, key);
     }
 
     @Override
@@ -52,8 +59,9 @@ public class MenuState extends GameState {
         shutDownImage.render(g);
 
 
-        DrawText dt=new DrawText("TimesRoman","Hello World", Font.PLAIN, 100,new Object2DInt(100,100,0,0),new Color(196, 3, 3, 148));
+        DrawText dt=new DrawText("TimesRoman","Hello World", Font.PLAIN, 100,new Object2DInt(100,100,0,0),new Color(0,0,0,255));
         dt.render(g);
+        sf.render(g);
     }
 
 }
